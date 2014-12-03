11ec7fe09dfd8408f80afe3695abd7b17aec135a
tpluscode <tpluscode@users.noreply.github.com>
2014-12-03 19:27:18 +0100

    body of main

--- Program.cs
+++ Program.cs
@@ -16,9 +16,15 @@ Also here are the required namespaces:
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using RomanticWeb;
+using RomanticWeb.DotNetRDF;
 using RomanticWeb.Entities;
 using RomanticWeb.Mapping.Attributes;
 using RomanticWeb.Mapping.Fluent;
+using RomanticWeb.Ontologies;
+using VDS.RDF;
+using VDS.RDF.Writing;
 
 /**
 ## Model and mappings
@@ -45,7 +51,7 @@ public interface IPerson : IEntity
 Here's the publication entity mapped with [Dublin Core][dc].
 **/
 
-public interface IPublication
+public interface IPublication : IEntity
 {
     string Title { get; set; }
 
@@ -66,10 +72,44 @@ public class PublicationMapping : EntityMap<IPublication>
     }
 }
 
+/**
+Now everyting is set to execute sample code, which will query and modify some data.
+**/
+
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
+        ITripleStore store = new TripleStore();
+        store.LoadFromFile("input.trig");
+
+        var factory = new EntityContextFactory();
+
+        // it is also possible to add fluent/attribute mappings separately
+        factory.WithMappings(mb => mb.FromAssemblyOf<IPerson>());
+
+        factory.WithMetaGraphUri(new Uri("http://romanticweb.net/samples"));
+
+        // this API bound to change in the future
+        factory.WithOntology(new DefaultOntologiesProvider(BuiltInOntologies.DCTerms | BuiltInOntologies.FOAF));
+
+        factory.WithDotNetRDF(store);
+
+        using (var context = factory.CreateContext())
+        {
+            foreach (var person in context.AsQueryable<IPerson>().Where(p => p.Name == "Tim"))
+            {
+                var pub = context.Create<IPublication>("http://romanticweb.net/samples/publication/" + Guid.NewGuid());
+                pub.Title = string.Format("Publication about RDF by {0} {1}", person.Name, person.LastName);
+                pub.DatePublished = DateTime.Now;
+
+                person.Publications.Add(pub);
+            }
+
+            context.Commit();
+        }
+
+        store.SaveToFile("output.trig", new TriGWriter());
     }
 }
 
