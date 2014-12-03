8dd3b24d60eac746d00f1419ccc658acbd46d7ae
tpluscode <tpluscode@users.noreply.github.com>
2014-12-03 18:49:38 +0100

    initial code

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿/**
+![logo](http://romanticweb.net/images/logo-small.png)
+
+# Romantic Web Sample
+
+This project is a small app, which demonstrates the use of [Romantic Web](http://romanticweb.net), the RDF mapper for .NET.
+
+What you will read below is actual source code from the [Program.cs](http://github.com/makolab/romanticweb.sample/Program.cs) file.
+
+## Model and mappings
+
+First, let's create some model interfaces and mappings. First we'll define a person entity, mapped with [FoaF][foaf] terms.
+
+and some associated 
+publications, which will be modelled with [Dublin Core][dc].
+**/
+
+public interface IPerson
+{
+    
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    }
+}
+
+/**
+[foaf]: http://xmlns.com/foaf/spec/
+[dc]: purl.org/dc/terms/
+**/
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("RomanticWeb.Sample")]
+[assembly: AssemblyCopyright("Copyright MakoLab © 2013 - 2014")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
